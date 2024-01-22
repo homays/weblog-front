7af7e2279d7c199bdf2d1f145de2b86f8a3a3498
Arrebol <2362552845@qq.com>
2024-01-22 16:04:40 +0800

    feat: 整合全局状态管理库 Pinia

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import router from '@/router/index'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 导入全局路由守卫
 import '@/permission'
+// 引入全局状态管理 Pinia
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
@@ -18,6 +20,10 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 
+const pinia = createPinia()
+
 // 应用路由
 app.use(router)
+// 应用 Pinia
+app.use(pinia)
 app.mount('#app')
