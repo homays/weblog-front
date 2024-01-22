ce0632b3fe77515046589774fc083bd8fa3f821d
Arrebol <2362552845@qq.com>
2024-01-22 15:47:28 +0800

    feat: 后台公共左侧 Menu 菜单栏：样式布局

--- src/layouts/admin/components/AdminMenu.vue
+++ src/layouts/admin/components/AdminMenu.vue
@@ -2,46 +2,95 @@
     <div class="bg-slate-800 h-screen text-white">
         <!-- 顶部 Logo, 指定高度为 64px, 和右边的 Header 头保持一样高 -->
         <div class="flex items-center justify-center h-[64px]">
-            Logo
+            <img src="@/assets/weblog-logo.png" class="h-[60px]">
         </div>
 
         <!-- 下方菜单 -->
-        <el-menu default-active="2" class="el-menu-vertical-demo">
-            <el-sub-menu index="1">
-                <template #title>
+        <el-menu :default-active="defaultActive" @select="handleSelect">
+            <template v-for="(item, index) in menus" :key="index">
+                <el-menu-item :index="item.path">
                     <el-icon>
-                        <location />
+                        <!-- 动态图标 -->
+                        <component :is="item.icon"></component>
                     </el-icon>
-                    <span>Navigator One</span>
-                </template>
-                <el-menu-item-group title="Group One">
-                    <el-menu-item index="1-1">item one</el-menu-item>
-                    <el-menu-item index="1-2">item two</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group Two">
-                    <el-menu-item index="1-3">item three</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="1-4">
-                    <template #title>item four</template>
-                    <el-menu-item index="1-4-1">item one</el-menu-item>
-                </el-sub-menu>
-            </el-sub-menu>
-            <el-menu-item index="2">
-                <el-icon><icon-menu /></el-icon>
-                <span>Navigator Two</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-                <el-icon>
-                    <document />
-                </el-icon>
-                <span>Navigator Three</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <el-icon>
-                    <setting />
-                </el-icon>
-                <span>Navigator Four</span>
-            </el-menu-item>
+                    <span>{{ item.name }}</span>
+                </el-menu-item>
+            </template>
         </el-menu>
-</div>
+    </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const menus = [
+    {
+        'name': '仪表盘',
+        'icon': 'Monitor',
+        'path': '/admin/index'
+    },
+    {
+        'name': '文章管理',
+        'icon': 'Document',
+        'path': '/admin/article/list',
+    },
+    {
+        'name': '分类管理',
+        'icon': 'FolderOpened',
+        'path': '/admin/category/list',
+    },
+    {
+        'name': '标签管理',
+        'icon': 'PriceTag',
+        'path': '/admin/tag/list',
+    },
+    {
+        'name': '博客设置',
+        'icon': 'Setting',
+        'path': '/admin/blog/setting',
+    },
+]
+
+const route = useRoute()
+const router = useRouter()
+
+// 根据路由地址判断哪个菜单被选中
+const defaultActive = ref(route.path)
+
+// 菜单选择事件
+const handleSelect = (path) => {
+    router.push(path)
+}
+
+</script>
+<style>
+.el-menu {
+    background-color: rgb(30 41 59 / 1);
+    border-right: 0;
+}
+
+.el-sub-menu__title {
+    color: #fff;
+}
+
+.el-sub-menu__title:hover {
+    background-color: #ffffff10;
+}
+
+/* .el-menu-item.is-active {
+    background-color: var(--el-color-primary);
+    color: #fff;
+}
+
+.el-menu-item.is-active:hover {
+    background-color: var(--el-color-primary);
+} */
+
+.el-menu-item {
+    color: #fff;
+}
+
+.el-menu-item:hover {
+    background-color: #ffffff10;
+}
+</style>
