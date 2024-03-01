7ad44c8e4b451cdb9b8a41a620cce0065efab4f2
Arrebol <2362552845@qq.com>
2024-03-01 16:01:44 +0800

    feat: 后端管理功能补充：文章预览跳转、前台首页跳转

--- src/pages/admin/article-list.vue
+++ src/pages/admin/article-list.vue
@@ -31,6 +31,7 @@
 
             <!-- 分页列表 -->
             <el-table :data="tableData" border stripe style="width: 100%" v-loading="tableLoading">
+                <el-table-column prop="id" label="ID" width="50" />
                 <el-table-column prop="title" label="标题" width="380" />
                 <el-table-column prop="cover" label="封面" width="180">
                     <template #default="scope">
@@ -45,6 +46,11 @@
                                 <Edit />
                             </el-icon>
                             编辑</el-button>
+                        <el-button size="small" @click="goArticleDetailPage(scope.row.id)">
+                            <el-icon class="mr-1">
+                                <View />
+                            </el-icon>
+                            预览</el-button>
                         <el-button type="danger" size="small" @click="deleteArticleSubmit(scope.row)">
                             <el-icon class="mr-1">
                                 <Delete />
@@ -206,6 +212,9 @@ import moment from 'moment'
 import { showMessage, showModel } from '@/composables/util'
 import { MdEditor } from 'md-editor-v3'
 import 'md-editor-v3/lib/style.css'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 // 模糊搜索的文章标题
 const searchArticleTitle = ref('')
@@ -358,6 +367,11 @@ const rules = {
     cover: [{ required: true }],
     categoryId: [{ required: true, message: '请选择文章分类', trigger: 'blur' }],
     tags: [{ required: true, message: '请选择文章标签', trigger: 'blur' }],
+}
+
+// 跳转文章详情页
+const goArticleDetailPage = (articleId) => {
+    router.push('/article/' + articleId)
 }
 
 // 上传文章封面图片
